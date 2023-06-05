d0e9d20f785c12f786e969c2d1022627555caa64
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-06-05 22:17:26 +0300

    added skeleton for loading, added rendered users

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,14 +1,35 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
-
+import Skeleton from "./Skeleton";
 function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
+
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
-  return "users list";
+  if (isLoading) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return renderedUsers;
 }
 
 export default UsersList;
