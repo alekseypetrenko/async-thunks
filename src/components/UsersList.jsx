4c68ca4e17dd78f7a3b2b27ca7ca6ce02a8680c8
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2023-06-07 23:58:04 +0300

    added addUser thunk

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
+
 function UsersList() {
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => {
@@ -11,6 +13,10 @@ function UsersList() {
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
+
+  const handleUserAdd = () => {
+    dispatch(addUser());
+  };
 
   if (isLoading) {
     return <Skeleton times={6} className="h-10 w-full" />;
@@ -29,7 +35,15 @@ function UsersList() {
     );
   });
 
-  return renderedUsers;
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button onClick={handleUserAdd}>+ Add User</Button>
+      </div>
+      {renderedUsers}
+    </div>
+  );
 }
 
 export default UsersList;
